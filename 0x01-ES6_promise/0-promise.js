82652a010e6aa991836688c82c982b57684fa8e7
root <ejimovc@gmail.com>
2024-07-27 04:19:07 +0000

    0x01-ES6_promise/ AND 0x02-ES6_classes/ UPLOAD

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const success = true; // Simulate success or failure
-
-    if (success) {
-      resolve('Data fetched successfully!');
-    } else {
-      reject(new Error('Failed to fetch data.'));
-    }
-  });
+	return new Promise((resolve, reject) => {});
 }
