f0c068e3b7795fcfc2b9573c503b75654a0d1cb8
root <ejimovc@gmail.com>
2024-07-30 14:54:39 +0000

    All file done and ready for check

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,10 @@
-export default function cleanSet(set, startString) {
-  if (startString === '') return '';
-
-  return [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
+/* eslint-disable array-callback-return */
+export default function cleanSet(set, string) {
+    if (string === undefined || string.length === 0) {
+        return '';
+    }
+    return [...set]
+        .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+        .map((str) => (str !== undefined ? str.slice(string.length) : ''))
+        .join('-');
 }
