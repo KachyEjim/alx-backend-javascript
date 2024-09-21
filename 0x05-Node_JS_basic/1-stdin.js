6593fe8dfc88eb86612bcbb3e98559e7046c216f
kachyEjim <ejimovcgmail.com>
2024-09-21 04:58:16 +0100

    All task done in 0x06-unittests_in_js

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+
+const readline = require('readline');
+
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+read.question('Welcome to Holberton School, what is your name?\n', function (name) {
+    console.log(`Your name is: ${name}`);
+    read.close();
+})
+
+read.on('close', function () {
+    console.log('This important software is now closing');
+    process.exit(0);
+})
---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+    try {
+        const data = await fs.readFile(path, 'utf8');
+
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+
+        const header = lines.shift();
+
+        const fieldGroups = {};
+
+        for (const line of lines) {
+            const student = line.split(',');
+
+            if (student.length === 4) {
+                const firstName = student[0];
+                const field = student[3];
+
+                if (!fieldGroups[field]) {
+                    fieldGroups[field] = [];
+                }
+                fieldGroups[field].push(firstName);
+            }
+        }
+
+        const totalStudents = Object.values(fieldGroups).reduce((sum, students) => sum + students.length, 0);
+        console.log(`Number of students: ${totalStudents}`);
+
+        for (const [field, students] of Object.entries(fieldGroups)) {
+            console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+        }
+
+    } catch (error) {
+        throw new Error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
